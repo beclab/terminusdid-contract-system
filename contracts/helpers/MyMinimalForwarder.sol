94d44d309663d5ecb0374d5eed2f76a20547790a
Tang Wei <tw7613781@gmail.com>
2023-09-12 21:44:57 +0800

    chore: init v2 project without submodule yet

--- contracts/helpers/MyMinimalForwarder.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.16;
-
-import {MinimalForwarder} from "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
-
-contract MyMinimalForwarder is MinimalForwarder {}
--- scripts/tasks/bytes16.js
+++
@@ -1,4 +0,0 @@
-const {ethers} = require("hardhat");
-
-let l = ethers.utils.formatBytes32String("test")
-console.log(l.substring(0,34))
