3b442f8875b596396c038822eb287718fb61ad75
Tang Wei <tw7613781@gmail.com>
2023-09-15 10:26:36 +0800

    chore: clear code

--- src/Resolver/RsaResolver.sol
+++ src/Resolver/RsaResolver.sol
@@ -1,20 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-import {BaseResolver} from "./BaseResolver.sol";
+contract RsaResolver {
 
-abstract contract RsaResolver is BaseResolver {
-    string constant KEY = "std:rsaPubKey";
-
-    event RsaChanged(bytes32 indexed node, string key, string pubKey);
-
-    function setRsaPubKey(bytes32 node, string calldata pubKey) external {
-        oracle.setExtentedAttr(node, KEY, bytes(pubKey));
-        emit RsaChanged(node, KEY, pubKey);
-    }
-
-    function getRsaPubKey(bytes32 node) external view returns (string memory) {
-        bytes memory pubKey = oracle.getExtentedAttr(node, KEY);
-        return string(pubKey);
+    function isValidRsaPubKey(bytes calldata pubKey) public pure returns (bool) {
+        return true;
     }
 }
