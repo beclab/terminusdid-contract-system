3b68299d4bac91961c4d9cf421d5adcb3a5e27b5
dlspn <115976846+dlspn@users.noreply.github.com>
2023-09-21 17:38:41 +0800

    fill up domain level trace & some fix

--- src/Permissions.sol
+++ src/Permissions.sol
@@ -8,7 +8,7 @@ library Permissions {
     using DomainUtils for string;
     using DomainUtils for DomainUtils.Slice;
 
-    using {_isController} for TerminusDID;
+    using {_getOwner} for TerminusDID;
 
     uint64 internal constant RESERVED_KEY_MAX = 0xffff;
 
@@ -16,46 +16,42 @@ library Permissions {
 
     error NonexistentDomain();
 
-    function allowSetTag(TerminusDID registry, address addr, string memory domain, bytes8 key)
+    function allowSetTag(TerminusDID registry, address auth, DomainUtils.Slice[] memory levels, bytes8 key)
         internal
         view
         returns (bool)
     {
-        (bool isController, uint256 depth) = registry._isController(addr, domain);
-        if (!isController) {
-            return false;
-        }
-        if (depth == 0) {
-            return true;
-        }
-        return uint64(key) <= RESERVED_KEY_MAX;
-    }
-
-    function allowRegister(TerminusDID registry, address addr, string memory domain) internal view returns (bool) {
-        (bool isController,) = registry._isController(addr, domain);
-        return isController;
-    }
-
-    function _isController(TerminusDID registry, address addr, string memory fullDomain)
-        private
-        view
-        returns (bool ok, uint256 depth)
-    {
-        for (DomainUtils.Slice memory domain = fullDomain.asSlice(); !domain.isEmpty(); ++depth) {
-            try registry.ownerOf(domain.tokenId()) returns (address owner) {
-                if (owner == addr) {
-                    return (true, depth);
+        for (uint256 i = 0; i < levels.length; ++i) {
+            if (registry._getOwner(levels[i].tokenId()) == auth) {
+                if (i == 0) {
+                    return true;
                 }
-            } catch (bytes memory reason) {
-                if (reason.length == 36 && bytes4(reason) == _sigERC721NonexistentToken) {
-                    revert NonexistentDomain();
-                } else {
-                    assembly {
-                        revert(add(32, reason), mload(reason))
-                    }
+                return uint64(key) <= RESERVED_KEY_MAX;
+            }
+        }
+        return false;
+    }
+
+    function allowRegister(TerminusDID registry, address auth, string memory domain) internal view returns (bool) {
+        for (DomainUtils.Slice memory ds = domain.asSlice(); !ds.isEmpty(); ds.detachSub()) {
+            if (registry._getOwner(ds.tokenId()) == auth) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function _getOwner(TerminusDID registry, uint256 tokenId) private view returns (address) {
+        try registry.ownerOf(tokenId) returns (address owner) {
+            return owner;
+        } catch (bytes memory reason) {
+            if (reason.length == 36 && bytes4(reason) == _sigERC721NonexistentToken) {
+                revert NonexistentDomain();
+            } else {
+                assembly {
+                    revert(add(32, reason), mload(reason))
                 }
             }
-            domain.detachSub();
         }
     }
 }
