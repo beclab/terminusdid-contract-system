f5b39ac318321adb3c0b9749ccc310ddf2698f78
Tang Wei <tw7613781@gmail.com>
2023-09-27 16:09:45 +0800

    chore: deploy script

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -9,6 +9,8 @@ import {Registrar} from "../src/Registrar.sol";
 contract DeployScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address domainOwner = vm.envAddress("SNOWINNING_COM_OWNER");
+
         vm.startBroadcast(deployerPrivateKey);
 
         PublicResolver resolver = new PublicResolver();
@@ -18,6 +20,9 @@ contract DeployScript is Script {
         string memory _symbol = "TTDID";
         TerminusDID registry = new TerminusDID(_name, _symbol, address(registrar));
         registrar.setRegistry(address(registry));
+
+        registrar.registerTLD("com", "did", domainOwner);
+        registrar.register("snowinning", "com", "did", domainOwner, TerminusDID.Kind.Organization);
 
         vm.stopBroadcast();
     }
